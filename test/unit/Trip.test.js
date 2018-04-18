e6a3885732068c7a7589772eca2b662a8332ed14
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-18 09:38:44 -0700

    started writing Trip test

--- test/unit/Trip.test.js
+++ test/unit/Trip.test.js
@@ -2,5 +2,25 @@ const { assert } = require('chai');
 const Trip = require('../../lib/models/Trip');
 
 describe('Trip model', () => {
+    it('vaild good model', () => {
+        const data = {
+            location: {
+                park: 'Ecola State Park',
+                trail: 'Tillamook Head Trail',
+                trailhead: 'Tillamook Head Trailhead'
+            },
+            style: 'backpacking',
+            details: {
+                length: 6,
+                elevation: 1350,
+                days: 2,
+                nights: 1
+            },
+            season: 'spring',
+            campers: 2,
+            gear: ['tent', 'backpack', 'sleep system', 'clothes', 'food', 'cook system']
+        };
+    });
+
 
 });
