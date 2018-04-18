4ef0b87df88486fd14cea1da4e3ac39afaae1395
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-18 13:10:47 -0700

    fixed typo

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -9,7 +9,7 @@ module.exports = {
     dropCollection(name) {
         return mongoose.connection.dropCollection(name)
             .catch(err => {
-                if(err.codeName !== 'NameSpaceNotFound') throw err;
+                if(err.codeName !== 'NamespaceNotFound') throw err;
             });
     }
 };
