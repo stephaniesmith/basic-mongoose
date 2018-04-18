355809c3fe6d2f08ed3b277501ed80f52de68598
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-18 11:07:12 -0700

    updated to trips db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connect = require('./lib/connect');
 
 const PORT = process.env.PORT || 3000;
 
-const MONGOD_URI = process.env.MONGOD_URI || 'mongodb://localhost:27017/routes';
+const MONGOD_URI = process.env.MONGOD_URI || 'mongodb://localhost:27017/trips';
 
 connect(MONGOD_URI);
 
