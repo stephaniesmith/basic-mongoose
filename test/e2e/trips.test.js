1025fa177e81ab881487b39eaa5ef13646ca60e7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-18 10:49:49 -0700

    wrote post test

---
+++ test/e2e/trips.test.js
@@ -0,0 +1,61 @@
+const { assert } = require('chai');
+const request = require('./request');
+const Trip = require('../../lib/models/Trip');
+const { dropCollection } = require('./db');
+
+describe('Trip API', () => {
+    
+    before(() => dropCollection('trips'));
+
+    let tillamook = {
+        location: {
+            park: 'Ecola State Park',
+            trail: 'Tillamook Head Trail',
+            trailhead: 'Tillamook Head Trailhead'
+        },
+        style: 'backpacking',
+        details: {
+            length: 6,
+            elevation: 1350,
+            days: 2,
+            nights: 1
+        },
+        season: 'spring',
+        campers: 2,
+        gear: ['tent', 'backpack', 'sleep system', 'clothes', 'food', 'cook system']
+    };
+
+    // let opal = {
+    //     location: {
+    //         park: 'Opal Creek Wilderness',
+    //         trail: 'Opal Creek Trail',
+    //         trailhead: 'Opal Creek TrailHead'
+    //     },
+    //     style: 'backpacking',
+    //     details: {
+    //         length: 10.5,
+    //         elevation: 1240,
+    //         days: 2,
+    //         nights: 1
+    //     },
+    //     season: 'summer',
+    //     campers: 3,
+    //     gear: ['tents', 'backpacks', 'sleep systems', 'clothes', 'food', 'cook systems']
+    // };
+
+    it('saves a trip', () => {
+        return new Trip(tillamook).save()
+            .then(saved => {
+                saved = saved.toJSON();
+                assert.ok(saved._id);
+            });
+    });
+
+    // it('saves a trip', () => {
+    //     return request.post('/trips')
+    //         .send(tillamook)
+    //         .then(({ body }) => 
+    //             assert.ok(body._id)
+    //         );
+    // });
+});
